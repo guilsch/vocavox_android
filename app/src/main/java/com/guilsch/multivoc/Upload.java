e6bf68d7ba644bd6f509925de622ad8ca0565700
gsch <gsch@github.com>
2022-09-12 20:00:12 -0400

    starting...

--- app/src/main/java/com/guilsch/multivoc/Upload.java
+++ app/src/main/java/com/guilsch/multivoc/Upload.java
@@ -1,4 +1,18 @@
 package com.guilsch.multivoc;
 
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.services.drive.DriveScopes;
+
 public class Upload {
+
+    private void requestSignIn() {
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                .build();
+
+//        GoogleSignInClient client = GoogleSignIn.getClient
+    }
 }

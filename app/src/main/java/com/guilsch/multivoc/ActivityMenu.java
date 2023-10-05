2963c70f2eaf25901f2e40a128370949c21b15bd
gsch <gsch@github.com>
2023-10-05 23:18:31 +0200

    new header, add image menu zoom

--- app/src/main/java/com/guilsch/multivoc/ActivityMenu.java
+++ app/src/main/java/com/guilsch/multivoc/ActivityMenu.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.pixelcan.inkpageindicator.InkPageIndicator;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +18,9 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -25,28 +29,28 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ActivityMenu extends AppCompatActivity  {
 
+    private static boolean isZoomed = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu2);
 
         ViewPager viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tabs);
 
-        FragmentPagerAdapter adapter = createFragmentPagerAdapter();
+        AdapterMenuPager adapter = new AdapterMenuPager(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "Tab 1");
+        adapter.addFragment(new Tab2Fragment(), "Tab 2");
+
         viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
 
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+        InkPageIndicator inkPageIndicator = (InkPageIndicator) findViewById(R.id.indicator);
+        inkPageIndicator.setViewPager(viewPager);
 
         Utils.printNBCards();
-
-        // Fragments management
-        if (getIntent().hasExtra("FRAG_INDEX")) {
-            int fragmentIndex = getIntent().getIntExtra("FRAG_INDEX", 1);
-            viewPager.setCurrentItem(fragmentIndex);
-        }
     }
-
 
     public static class Tab1Fragment extends Fragment {
         @SuppressLint("ClickableViewAccessibility")
@@ -62,6 +66,7 @@ public class ActivityMenu extends AppCompatActivity  {
             LinearLayout trainLayout = view.findViewById(R.id.train_layout);
             LinearLayout learnLayout = view.findViewById(R.id.learn_layout);
             TextView cardsToTrainNB = view.findViewById(R.id.cards_to_train_nb);
+            ImageView menuImageView = view.findViewById(R.id.menu_image_view);
 
             // Set variables
             flag.setImageDrawable(Param.FLAG_ICON_TARGET);
@@ -72,6 +77,9 @@ public class ActivityMenu extends AppCompatActivity  {
             trainLayout.setOnClickListener(v -> parentActivity.preChangeToTrainActivity());
             learnLayout.setOnClickListener(v -> parentActivity.preChangeToLearnActivity());
             flag.setOnClickListener(v -> parentActivity.changeActivity(ActivityMain.class));
+
+            // Image zoom init
+            isZoomed = false;
 
             // On touch
             flag.setOnTouchListener(new View.OnTouchListener() {
@@ -89,8 +97,123 @@ public class ActivityMenu extends AppCompatActivity  {
                 }
             });
 
+            menuImageView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            // L'utilisateur a appuyé sur l'image
+                            if (!isZoomed) {
+                                zoomImageIn(menuImageView, 1.2f, 1000);
+                                isZoomed = true;
+                            }
+                            break;
+                        case MotionEvent.ACTION_UP:
+                        case MotionEvent.ACTION_CANCEL:
+                            // L'utilisateur a relâché l'image
+                            if (isZoomed) {
+                                zoomImageOut(menuImageView, 1.2f, 300);
+                                isZoomed = false;
+                            }
+                            break;
+                    }
+                    return true;
+                }
+            });
+
+//            trainLayout.setOnTouchListener(new View.OnTouchListener() {
+//                @Override
+//                public boolean onTouch(View v, MotionEvent event) {
+//                    switch (event.getAction()) {
+//                        case MotionEvent.ACTION_DOWN:
+//                            // L'utilisateur a appuyé sur l'image
+//                            if (!isZoomed) {
+//                                zoomImageIn(trainLayout, 1.05f, 100);
+//                                isZoomed = true;
+//                            }
+//                            break;
+//                        case MotionEvent.ACTION_UP:
+//                        case MotionEvent.ACTION_CANCEL:
+//                            // L'utilisateur a relâché l'image
+//                            if (isZoomed) {
+//                                zoomImageOut(trainLayout, 1.05f, 100);
+//                                isZoomed = false;
+//                            }
+//                            break;
+//                    }
+//                    return true;
+//                }
+//            });
+
+//            trainLayout.setOnTouchListener(new View.OnTouchListener() {
+//                private float startX, startY;
+//                private boolean isZoomed = false;
+//
+//                @Override
+//                public boolean onTouch(View v, MotionEvent event) {
+//                    switch (event.getAction()) {
+//                        case MotionEvent.ACTION_DOWN:
+//                            startX = event.getX();
+//                            startY = event.getY();
+//                            break;
+//                        case MotionEvent.ACTION_UP:
+//                            float endX = event.getX();
+//                            float endY = event.getY();
+//
+//                            float deltaX = Math.abs(endX - startX);
+//                            float deltaY = Math.abs(endY - startY);
+//
+//                            // Vérifiez si le mouvement est suffisamment petit pour être considéré comme un clic
+//                            if (deltaX < 10 && deltaY < 10) {
+//                                // C'est un clic
+//                                if (!isZoomed) {
+//                                    // Exécutez votre logique de zoom ici si nécessaire
+//                                    zoomImageIn(trainLayout, 1.05f, 100);
+//                                    isZoomed = true;
+//                                } else {
+//                                    // Si déjà agrandi, exécutez votre logique de clic ici
+//                                    parentActivity.preChangeToTrainActivity();
+//                                }
+//                            } else {
+//                                // C'est un mouvement tactile, par exemple un balayage
+//                                if (isZoomed) {
+//                                    // Exécutez votre logique de zoom-out ici si nécessaire
+//                                    zoomImageOut(trainLayout, 1.05f, 100);
+//                                    isZoomed = false;
+//                                }
+//                            }
+//                            break;
+//                    }
+//                    return true;
+//                }
+//            });
+
             return view;
         }
+    }
+
+    private static void zoomImageIn(View view, float scale, int speed) {
+        Animation animation = new ScaleAnimation(
+                1f, scale,
+                1f, scale,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setDuration(speed);
+        animation.setFillAfter(true);
+
+        view.startAnimation(animation);
+    }
+
+    private static void zoomImageOut(View view, float scale, int speed) {
+        Animation animation = new ScaleAnimation(
+                scale, 1f,
+                scale, 1f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setDuration(speed);
+        animation.setFillAfter(true);
+
+        view.startAnimation(animation);
     }
 
     public static class Tab2Fragment extends Fragment {
